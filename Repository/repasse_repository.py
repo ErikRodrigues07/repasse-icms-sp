3f6fac50f2777625a5ccea65853f31a225ce1d97
ErikRodrigues07 <43653449+ErikRodrigues07@users.noreply.github.com>
2026-09-23 19:12:26 -0300

    Update repasse_repository.py

--- Repository/repasse_repository.py
+++ Repository/repasse_repository.py
@@ -4,6 +4,17 @@ class RepasseRepository:
         self.conn = conn
         self.cursor = conn.cursor()
 
+    def inserir_municipio(self, codigo, nome, uf="SP"):
+        self.cursor.execute("""
+            IF NOT EXISTS (
+                SELECT 1 FROM Municipios WHERE Codigo = ?
+            )
+            BEGIN
+                INSERT INTO Municipios (Codigo, Nome, UF)
+                VALUES (?, ?, ?)
+            END
+        """, (codigo, codigo, nome, uf))
+
     def inserir_mensal(self, dados):
         self.cursor.execute("""
             INSERT INTO RepasseMensal (
@@ -23,3 +34,12 @@ class RepasseRepository:
 
     def commit(self):
         self.conn.commit()
+
+    def municipio_existe(self, codigo):
+        self.cursor.execute("""
+            SELECT 1
+            FROM Municipios
+            WHERE Codigo = ?
+        """, (codigo,))
+
+        return self.cursor.fetchone() is not None
