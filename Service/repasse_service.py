1abe611d5b91da1f7b4c1f8ebb70ca7d14d7b326
ErikRodrigues07 <43653449+ErikRodrigues07@users.noreply.github.com>
2026-09-23 19:13:30 -0300

    Update repasse_service.py

--- Service/repasse_service.py
+++ Service/repasse_service.py
@@ -3,6 +3,9 @@ class RepasseService:
     def __init__(self, repository):
         self.repository = repository
 
+    def salvar_municipio(self, codigo, nome):
+        self.repository.inserir_municipio(codigo, nome)
+
     def salvar_mensal(self, codigo, ano, mes, icms, ipva, fundexp, comp, total):
         dados = (codigo, ano, mes, icms, ipva, fundexp, comp, total)
         self.repository.inserir_mensal(dados)
@@ -10,3 +13,6 @@ class RepasseService:
     def salvar_detalhe(self, codigo, ano, mes, periodo, data, icms, fundexp, comp, ipva, total):
         dados = (codigo, ano, mes, periodo, data, icms, fundexp, comp, ipva, total)
         self.repository.inserir_detalhe(dados)
+        
+    def municipio_existe(self, codigo):
+        return self.repository.municipio_existe(codigo)
